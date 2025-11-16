fe61f3e0ef21a838e52199cd0bfbddfe1bfce1f0
anthophilla <szymonskiba846@gmail.com>
2025-11-16 15:02:03 +0100

    added a basic command handler

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ with open('config.json', "r") as f:
 class AI:
     instructions="""you're a kitchen assistant ready to bring up instructions any time.
     The user is talking to you through a device equiped with a microphone, camera and speakers whith which you communicate with him.
-    If the user requests to change the volume PREFIX the string with [volume] like this '[volume +5%]content of your response' (the 5% can be replaced with -5% or any integer)"""
+    If the user requests to change the volume PREFIX the string with [vol*%] like this '[vol+5%]content of your response' (the +5% can be replaced with -5% or any integer)"""
 
     def __init__(self, openai_key: str):
         self.client = OpenAI(api_key=openai_key)
@@ -21,10 +21,28 @@ class AI:
             input=inpt
         ).output_text
 
+# handle the "[command]"
+def handle_commands(commands: str):
+    i=0
+    match commands[i:(i+3)]:
+            case "vol": print("TODO!!!")
+
 def handle_text(text, args: (AI,)):
     ai=args[0]
     print(f"{type(text)}: {text}")
-    print(f"ai: {ai.ask(text)}")
+    #response = ai.ask(text)
+    response = "[vol+5%]content"
+    print(f"ai: {response}")
+    
+    content=response
+    if response[0]=='[': 
+        i=1
+        while response[i]!=']': i+=1
+        handle_commands(response[1:(i-1)])
+        content=response[(i+1):]
+
+    print(f"content: {content}")
+    
 
 def main():
     ai=AI(config["openai_key"])
