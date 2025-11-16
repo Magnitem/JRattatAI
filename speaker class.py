d50b1eb23c622ce8e0a89e43b4cbde75748a13ab
Magnitem <136966339+Magnitem@users.noreply.github.com>
2025-11-16 10:57:06 +0100

    Enhance speaker class with additional speech command
    Added a new speech command to the speaker class.

--- speaker class.py
+++ speaker class.py
@@ -6,11 +6,12 @@ class speaker:
         self.voices = voices
     def speak(text):
         engine = pyttsx3.init()
-        engine.setProperty('volume', 1.0)   #volume
-        engine.setProperty('rate', 120)    #rate
+        engine.setProperty('volume', 1.0)   #volume min 0 max 1
+        engine.setProperty('rate', 120)    #rate min 1 max 150
         voices = engine.getProperty('voices')
-        engine.setProperty('voice', voices[1].id)
+        engine.setProperty('voice', voices[1].id) #voices 0 - java voice, 1 - Italian acent voice, 2 - just a woman
         engine.say(text)
         engine.runAndWait()
 speaker.speak("Hello, I'am RattatAI, for shourt RT")
+
 speaker.speak("Today we are going to cook the pizza")
