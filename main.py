dd7fab8b4a81d390f6ba47071f2d59fa5c05f6f5
anthophilla <szymonskiba846@gmail.com>
2025-11-22 18:09:26 +0100

    not sure if working

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 import json
 
 from speech import SR
+import ai
 
 with open('config.json', "r") as f:
     config = json.load(f)
@@ -11,15 +12,12 @@ class AI:
     The user is talking to you through a device equiped with a microphone, camera and speakers whith which you communicate with him.
     If the user requests to change the volume PREFIX the string with [vol*%] like this '[vol+5%]content of your response' (the +5% can be replaced with -5% or any integer)"""
 
-    def __init__(self, openai_key: str):
-        self.client = OpenAI(api_key=openai_key)
+    def __init__(self, ai_model: str, api_key: str):
+        match ai_model:
+            case "chatgpt-4o": self.ai=ai.chatgpt("chatgpt-4o", self.instructions, api_key)
+            case _: raise Exception(f"no such ai model as {self.ai_model}")
 
-    def ask(self, inpt: str) -> str:
-        return self.client.responses.create(
-            model="o4-mini",
-            instructions=self.instructions,
-            input=inpt
-        ).output_text
+    def ask(self, inpt: str) -> str: return self.ai.ask(inpt)
 
 # handle the "[command]"
 def handle_commands(commands: str):
@@ -30,10 +28,12 @@ def handle_commands(commands: str):
 def handle_text(text, args: (AI,)):
     ai=args[0]
     print(f"{type(text)}: {text}")
+    #TODO
     #response = ai.ask(text)
-    response = "[vol+5%]content"
+    response: str = "[vol+5%]content"
     print(f"ai: {response}")
     
+    # seperate the commands from [*]
     content=response
     if response[0]=='[': 
         i=1
@@ -45,7 +45,7 @@ def handle_text(text, args: (AI,)):
     
 
 def main():
-    ai=AI(config["openai_key"])
+    ai=AI(config["ai-model"], config["api_key"])
     sr = SR()
     sr.listen(handle_text, (ai,))
 
