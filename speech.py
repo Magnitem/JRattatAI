6f18d8154be466f9705dd8bf4db283e8b11bc1cf
Magnitem <136966339+Magnitem@users.noreply.github.com>
2026-04-10 08:15:58 +0200

    Update speech.py

--- speech.py
+++ speech.py
@@ -1,24 +1,17 @@
 import speech_recognition as sr
 
 class SR:
-    def __init__(self):
-        self.r   = sr.Recognizer()
-        self.mic = sr.Microphone()
+    def listen()
+        listener = sr.Recognizer()
+        print("Gotów do słuchania")
         
-        with self.mic as source: self.r.adjust_for_ambient_noise(source)
-    # handler is a function taking string
-    def listen(self, handler, handler_args):
-        with self.mic as source:
-
-            while True:
-                print("listening!")
-                audio = self.r.listen(self.mic)
-                print("listened")
-                try:
-                    text = self.r.recognize_whisper(audio)
-                    handler(text, handler_args)
-                except sr.UnknownValueError:
-                    handler("cannot understand", handler_args)
-                except sr.RequestError as e:
-                    print("API UNAVAILABLE: ", e)
-                    handler("API unavailable: ", e)
+        with sr.Microphone() as micro:
+            listener.pause_treshhold = 1
+            input_speech = listener.listen(micro)
+        try
+            print("Czekam na komendy")
+            query = listener.recognise_google(input_speech, language="pl-Pl")
+            print("Youn said:",query)
+        except:
+            print("I don't understand")
+            print(exception)
